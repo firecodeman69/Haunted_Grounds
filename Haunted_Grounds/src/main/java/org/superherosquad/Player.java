3ae350142996501bd83ef5780896f93de7aa570b
Cody <cody199778@gmail.com>
2023-04-16 19:44:36 -0400

    Updated the Player class to have specific functionality around picking up, inspecting, dropping, and equipping items. Also created a help menu (which might need to go in a diff class) with a todo so we can get it to match the commands for the project

--- Haunted_Grounds/src/main/java/org/superherosquad/Player.java
+++ Haunted_Grounds/src/main/java/org/superherosquad/Player.java
@@ -1,6 +1,8 @@
 /**************** Cody ********************/
 package org.superherosquad;
 
+import java.util.ArrayList;
+
 public class Player extends Character {
 
     //ArrayList<Item> playerInventory;
@@ -17,7 +19,7 @@ public class Player extends Character {
     }
 
 //    public ArrayList<Item> getPlayerInventory() {
-//        return playerInventory;
+//        return playerInventory.toString;
 //    }
 
 //    public void addItemToInventory(Item item) {
@@ -26,6 +28,67 @@ public class Player extends Character {
 
 //    public void addItemsToInventory(ArrayList<Item> itemAL) {
 //        playerInventory.addAll(itemAL);
+//    }
+
+    /*******************Could be used instead of addItemToInventory***************/
+//    public String pickUpItem(Item item) {
+//        playerInventory.add(item);
+//        return (item.getName() + " was added to your inventory. Use command Inventory to see it now.");
+//    }
+
+
+//    public void dropItem(Item item) {
+//        for (Item i: equippedItems) {
+//          if(i.getName.equalsIgnoreCase(item)) {
+//              playerInventory.remove(item);
+//          }
+//        }
+//    }
+
+//        public void equipItem(Item item) {
+//        equippedItems.add(item);
+//    }
+
+//        public void unEquipItem(Item item) {
+//        for (Item i: equippedItems) {
+//          if(i.getName.equalsIgnoreCase(item)) {
+//              equippedItems.remove(item);
+//          }
+//        }
+//    }
+
+//    public String showInventory() {
+//        if (playerInventory != null) {
+//            return playerInventory.toString();
+//        } else {
+//            return ("You don't have any items in your inventory.");
+//        }
+//    }
+
+//    public Item explore(String name) {
+//        for (Item i : currentRoom.roomItems) {
+//            if (i.getName().equalsIgnoreCase(name)) {
+//                return i;
+//            }
+//        }
+//        return null;
+//    }
+
+    //todo: change the helpmenu to reflect the menu of our game
+//    public String helpMenu() {
+//        return """
+//                N: Allows the player to move north (if there is a room available).
+//                E: Allows the player to move east (if there is a room available).
+//                S: Allows the player to move south (if there is a room available).
+//                W: Allows the player to move west (if there is a room available).
+//                Explore: Check a room for items.
+//                Pickup{itemname}: Pick up (remove) an item from a room, and add it to your inventory.
+//                Drop{itemname}: Drop (remove) an item from your inventory and add it to current room.
+//                Inspect{itemname}: Return the description for an item in your inventory.
+//                Inventory: List all of the current items in your inventory.
+//                CurrentRoom: List the current room and the exits for that room.
+//                Quit: I hope you never want to, but this is your way out of the game if you wish.
+//                """;
 //    }
 
     public int getCurrentLocation() {
