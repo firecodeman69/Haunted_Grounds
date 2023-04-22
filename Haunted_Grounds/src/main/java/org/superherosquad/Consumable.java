342d9fe27720b05bf8395b04fd1571982830ce16
ReAnn <rhollins8199@yahoo.com>
2023-04-22 16:41:37 -0400

    Got newItem method working in Reader.java. Plus had to update the Item, Consumable, and Equipment java classes to appropriately match with the items.txt file tokens.

--- Haunted_Grounds/src/main/java/org/superherosquad/Consumable.java
+++ Haunted_Grounds/src/main/java/org/superherosquad/Consumable.java
@@ -19,4 +19,5 @@ public class Consumable extends Item {
     {
 
     }*/
+
 }
--- Haunted_Grounds/src/main/java/org/superherosquad/Item.java
+++ Haunted_Grounds/src/main/java/org/superherosquad/Item.java
@@ -103,4 +103,5 @@ public class Item {
                 + "[description:" + description + "] "
                 + "[price: $" + price + "] ";
     }
+
 }
