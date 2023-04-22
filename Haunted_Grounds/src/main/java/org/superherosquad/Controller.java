d64a663604b91883fbefadd5274b75baeb43e183
Cobi <jmiller51@ggc.edu>
2023-04-22 19:44:13 -0400

    Switched game and controller back. Set up the game & controller classes to have a functioning loop.

--- Haunted_Grounds/src/main/java/org/superherosquad/Controller.java
+++ Haunted_Grounds/src/main/java/org/superherosquad/Controller.java
@@ -1,223 +1,135 @@
 /**************** ReAnn ********************/
 package org.superherosquad;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controller {
-    private Scanner input;
-    private ArrayList<Room> gameRooms; //Cody
-    private ArrayList<Item> gameItems; //ReAnn
-    private ArrayList<Puzzle> gamePuzzles; //Cobi
-    private ArrayList<Monster> gameMonsters; //Cody
-    private ArrayList<NPC> gameNPCs; //Cobi
-    //Shop shop; //Cobi
-    private static final long serialVersionUID = 1L; //For the save game method
-    Player p = new Player();
-    Reader reader = new Reader();
+    private Scanner input = new Scanner(System.in);
+    private View view = new View();
+    private String invalid = "That is an invalid command.";
 
-    public void gamePlay(Controller c1) {
-        input = new Scanner(System.in);
-        String playerInput;
-        boolean playing = true;
-        boolean menuOpen = true;
-        while (playing) {
-            if (menuOpen) {
-                c1.mainMenu(c1);
-                menuOpen = false;
-            }
-            else {
-                System.out.println("Game begin!");
-                break;
-            }
-
-            playerInput = input.nextLine().toLowerCase();
-            switch (playerInput) {
-                case "north":
-                    System.out.println("Do the north thing.");
-                    if(p.currentRoom.getNorthRoom() != -1) p.setCurrentRoom(gameRooms.get(p.currentRoom.getNorthRoom()));
-                    else System.out.println("You can't go that way honey-boo-boo!");
-                case "south":
-                    System.out.println("Do the south thing.");
-                    if(p.currentRoom.getSouthRoom() != -1) p.setCurrentRoom(gameRooms.get(p.currentRoom.getSouthRoom()));
-                    else System.out.println("You can't go that way honey-boo-boo!");
-                case "east":
-                    System.out.println("Do the east thing.");
-                    if(p.currentRoom.getEastRoom() != -1) p.setCurrentRoom(gameRooms.get(p.currentRoom.getEastRoom()));
-                    else System.out.println("You can't go that way honey-boo-boo!");
-                case "west":
-                    System.out.println("Do the west thing.");
-                    if(p.currentRoom.getWestRoom() != -1) p.setCurrentRoom(gameRooms.get(p.currentRoom.getWestRoom()));
-                    else System.out.println("You can't go that way honey-boo-boo!");
-            }
-        }
+    public void gamePlay(ArrayList<Room> rooms, ArrayList<Item> items, ArrayList<Puzzle> puzzle, ArrayList<Monster> monsters, ArrayList<NPC> npcs, Player p, int mode) {
+    	String playerInput;
+    	String[] tokens;
+    	
+        switch (mode) {
+        /*
+         * First switch case checks the game mode.
+         * The way the game interprets commands is based on this switch statement.
+         */
+        	
+        	case 5: //Initial menu
+        	case 6: //Pause menu
+        		String mainhelp = 
+        		      """
+          			  List of main menu commands:
+          			  
+      	              "menuhelp" : Use this command to see a list of commands that can be used in the menu.
+      	              "newgame" : Use this command to create a new game and save file.
+      	              "newhard" : This command is the same as the new game command, but if the player dies during the game, the game is over and cannot be returned to.
+      	              "load " + save name : Use this command to load a save by inputting the previously determined save name.
+      	              "exit" : Use this command to leave the game.
+          			  """;
+        		
+        		String pausehelp = 
+    				  """
+        			  List of pause menu commands:
+        			  
+    	              "menuhelp" : Use this command to see a list of commands that can be used in the menu.
+    				  "continue" : Use this command to return to the current game.      	              
+    	              "newgame" : Use this command to create a new game and save file.
+    	              "newhard" : This command is the same as the new game command, but if the player dies during the game, the game is over and cannot be returned to.
+    	              "load " + save name : Use this command to load a save by inputting the previously determined save name.
+    	              "save " + save name : Use this command with a save name to save all progress. This command saves all information about the player and the map.
+    	              "exit" : Use this command to leave the game.
+        			  """;
+        		
+        		if(mode == 5) { //Welcoming messages for the main menu.
+        			view.print("Welcome to the Haunted Grounds game!\nMAIN MENU\n");
+        			view.print(mainhelp);
+        		}
+        		
+        		if(mode == 6) { //Welcoming messages for the pause menu.
+        			view.print("PAUSE MENU");
+        			view.print(pausehelp);
+        		}
+        		
+                playerInput = input.nextLine().toLowerCase(); //Parse user input
+                tokens = playerInput.split(" ");
+                
+        		switch(tokens[0]) { //Direct user input to the proper methods.
+		    		case "menuhelp": //Prints out the help message for the menu that the user is on.
+		    			if(mode == 5) {
+		    				view.print(mainhelp);
+		    			}
+		    			
+		    			if(mode == 6) {
+		    				view.print(pausehelp);
+		    			}
+		    			break;
+		    			
+		    		case "exit": //Exit the program.
+		    			view.print("Thanks for playing!");
+		    			System.exit(0);
+		    		
+		    		//TODO: Fix the continue command to where it brings you to the mode you were in before you entered the menu.
+		    		case "continue": //Return the user from the pause menu to their currently-active game.
+		    			if(mode == 6) {
+		    				mode = 0;
+		    			}
+		    			break;
+		    		
+		    		//TODO: Create the new game logic.
+		        	case "new": //Create a new game.
+		        		System.out.println("Starting a New Game!");
+		        		//new game logic
+		        		break;
+		        		
+		        	//TODO: Create the new hard game logic.
+		          	case "hard": //Create a new hard mode game.
+		          		System.out.println("Starting a New Hard-mode Game!");
+		            	//hard game logic 
+		          		break;
+		
+		          	//TODO: Create the new game logic.
+		        	case "load": //Load a game from a previous save file
+		        		System.out.println("Loading game!");
+		        		//load game
+		        		break;
+		        		
+		        	default: //This is reached if none of the previous cases were reached.
+		        		view.print(invalid);
+		        		break;
+        		}
+        		break;
+        		
+        	case 0:
+                playerInput = input.nextLine().toLowerCase();
+                tokens = playerInput.split(" ");
+                
+        		switch (tokens[0]) {
+		        case "north":
+		            System.out.println("Do the north thing.");
+		            if(p.currentRoom.getNorthRoom() != -1) p.setCurrentRoom(rooms.get(p.currentRoom.getNorthRoom()));
+		            else System.out.println("You can't go that way honey-boo-boo!");
+		            break;
+		        case "south":
+	                System.out.println("Do the south thing.");
+	                if(p.currentRoom.getSouthRoom() != -1) p.setCurrentRoom(rooms.get(p.currentRoom.getSouthRoom()));
+	                else System.out.println("You can't go that way honey-boo-boo!");
+	                break;
+	            case "east":
+	                System.out.println("Do the east thing.");
+	                if(p.currentRoom.getEastRoom() != -1) p.setCurrentRoom(rooms.get(p.currentRoom.getEastRoom()));
+	                else System.out.println("You can't go that way honey-boo-boo!");
+	                break;
+	            case "west":
+	                System.out.println("Do the west thing.");
+	                if(p.currentRoom.getWestRoom() != -1) p.setCurrentRoom(rooms.get(p.currentRoom.getWestRoom()));
+	                else System.out.println("You can't go that way honey-boo-boo!");
+	                break;
+        		}
+        }  
     }
-
-    public void mainMenu(Controller c1) {
-        // TODO: Implement game mechanics and logic here
-        input = new Scanner(System.in);
-        String playerInput;
-        System.out.println("Welcome to the Haunted Grounds game!");
-        boolean menuOpen = true;
-
-        while (menuOpen) {
-            System.out.println("Please enter a command.");
-            playerInput = input.nextLine().toLowerCase();
-            String[] commands = playerInput.split(" ");
-            System.out.println("You entered: " + playerInput);
-
-
-            //Main menu
-            switch (commands[0]) {
-                case "quit":
-                    System.out.println("Thanks for playing!");
-                    System.exit(0);
-                    break;
-                case "new":
-//                case "hard":
-//                    System.out.println("Starting a New Hard-mode Game!");
-//                    menuOpen = false;
-//                    //hard game logic
-//                    break;
-                    System.out.println("Starting a New Game!");
-                    c1.newGame(c1);
-//                    c1.gamePlay(c1);
-                    break;
-                case "load":
-                    System.out.println("Loading game!");
-                    c1.loadGame("playerSave.bin");
-                    break;
-            }
-            menuOpen = false;
-        }
-    }
-
-    public void newGame(Controller c1) {
-        gameRooms = reader.newRoom(); //Cody
-
-        gameItems = reader.newItem(); //ReAnn
-
-        gamePuzzles = reader.newPuzzle(); //Cobi
-
-        gameMonsters = reader.newMonster(); //Cody
-
-        gameNPCs = reader.newNPC(gamePuzzles); //Cobi
-
-        //shop = reader.newShop(); //Cobi
-        /***Cody***/
-        addItemToMonster();
-        addMonstersToRoom();
-        addItemToRoom();
-        addPuzzleToRoom();
-        addNPCToRoom();
-
-        p.setId(0);
-        p.setName("Character 1");
-        p.setHP(1000000);
-        p.setCurrency(9999999);
-        p.setDescription("First player of the game.");
-        p.setSpeed(250000);
-        p.setDefense(250000);
-        p.setAttack(2500000);
-        p.setCurrentRoom(gameRooms.get(0));
-        System.out.println(p);
-        System.out.println(p.getCurrentRoom());
-        c1.gamePlay(c1);
-        /****END***/
-    }
-
-    public void saveGame(Player p) { //Cody
-        ObjectOutputStream oos = null;
-        FileOutputStream fos;
-        try {
-            fos = new FileOutputStream("playerSave.bin");
-            oos = new ObjectOutputStream(fos); //Instantiate the ObjectOutputStream
-            oos.writeObject(p); //Write the object to the file
-        } catch (IOException ioe) {
-            System.out.println("IOException! Oh no!");
-        } finally { //close the stream even if there is an exception thrown
-            try {
-                if (oos != null) {
-                    oos.close();
-                }
-            } catch (IOException ioe) {
-                System.out.println("Closing the outputstream failed bucko");
-            }
-        }
-    }
-
-    public Player loadGame(String fileName) { //Cody
-        ObjectInputStream ois = null; //initialize a 'value' for ObejectInputStream
-        FileInputStream fis;
-        try {
-            fis = new FileInputStream(fileName);
-            ois = new ObjectInputStream(fis);
-            p = (Player) ois.readObject(); //set current player = the contents of the save file
-            //loadedGame = (Game) ois.readObject();
-        } catch (IOException | ClassNotFoundException ioe) { //multi catch statement instead of using 2 catch statements
-            System.out.println(fileName);
-            System.out.println("Either an IOException happened or the class couldn't be found! Youch!");
-        } finally { //close the stream even if there is an exception thrown
-            try {
-                if (ois != null) {
-                    ois.close();
-                }
-            } catch (IOException ioe) {
-                System.out.println("Closing the input Stream failed buckoo");
-            }
-        }
-        //return loadedGame;
-        return p;
-    }
-
-    public void addMonstersToRoom() { //Cody - adds monsters to rooms
-        for (Room r : gameRooms) {
-            for (Monster m : gameMonsters) {
-                if (r.getMonsterId() == m.getId()) {
-                    r.setMonster(m);
-                }
-            }
-        }
-    }//End of method
-
-    public void addPuzzleToRoom() { //Cody - adds puzzles to rooms
-        for (Room r : gameRooms) {
-            for (Puzzle p : gamePuzzles) {
-                if (r.getPuzzleId() == p.getId()) {
-                    r.setPuzzle(p);
-                }
-            }
-        }
-    }//End of method
-
-    public void addItemToRoom() { //Cody - adds items to rooms
-        for (Room r : gameRooms) {
-            for (Item i: gameItems) {
-                if (r.getItemId() == i.getId()) {
-                    r.addItem(i);
-                }
-            }
-        }
-    }//End of method
-
-    public void addItemToMonster() { //Cody - adds items to rooms
-        for (Monster m : gameMonsters) {
-            for (Item item: gameItems) {
-                for (int i = 0; i < m.getMonsterItemAssociations().length; i++) {
-                    if (m.getMonsterItemAssociations()[i] == item.getId()) m.addItems(item);
-                }
-            }
-        }
-    }//End of method
-
-    public void addNPCToRoom() { //Cody - adds puzzles to rooms
-        for (Room r : gameRooms) {
-            for (NPC npc : gameNPCs) {
-                if (r.getPuzzleId() == npc.getId()) {
-                    r.setNPC(npc);
-                }
-            }
-        }
-    }//End of method
 }
--- Haunted_Grounds/src/main/java/org/superherosquad/Player.java
+++ Haunted_Grounds/src/main/java/org/superherosquad/Player.java
@@ -14,7 +14,10 @@ public class Player extends Character implements Serializable {
 
     Room previousRoom;
 
-    public Player() {} //create a brand-new player
+    public Player() {
+    	super(0, "Character 1", 1000000, 9999999, "First player of the game.", 250000, 250000, 250000);
+    } //create a default player
+    
     public Player(int Id, String name, int hp, int currency, String description, //to be used for loading??
                   int speed, int defense, int attack,
                   Room currentRoom) {
@@ -97,7 +100,7 @@ public class Player extends Character implements Serializable {
         this.previousLocation = previousLocation;
     }
 
-    /****************Cody && ReAnn*********************/
+    /****************ReAnn*********************/
     public String helpMenu() {
         return """
                 =======================================================================================================================================================================================
@@ -106,15 +109,15 @@ public class Player extends Character implements Serializable {
                 MENU:
                 "menu" : When in the game, the user can use this command to display the main menu.
                 "menuhelp" : When in the main menu, the user can use this command to see information about the load and save features.
-                "new game" : When in the main menu, this command allows the user to create a new game and save file.
-                "new hard mode game" : When in the main menu, this command is the same as the new game command, but if the player dies during the game, the game is over and cannot be returned to.
+                "newgame" : When in the main menu, this command allows the user to create a new game and save file.
+                "newhard" : When in the main menu, this command is the same as the new game command, but if the player dies during the game, the game is over and cannot be returned to.
                 "load " + save name : When in the main menu, use this command to load a save by inputting the previously determined save name.
                 "save " + save name : When in the main menu, use this command with a save name to save all progress. This command saves all information about the player and the map.
                 "continue" : When in the main menu, this command returns the user back into the current game(only displayed if the user is already in a game).
                 "exit" : When in the main menu, the user can use this command to leave the game.
 
                 PLAYER:
-                "move" + direction : Command used to move in the specified direction of 'North', 'South', 'East', or 'West'.
+                "move " + direction : Command used to move in the specified direction of 'North', 'South', 'East', or 'West'.
                 "help" : When in the game, the user can use this command to display a list of commands followed by a brief description for each one.
                 "inspect " + item : Users can use this command to read a description of an item located in the room or inventory.
                 "inventory" : Command used to view all items currently in the player's inventory. Also, displays current items equipped.
@@ -130,21 +133,21 @@ public class Player extends Character implements Serializable {
                 "hint" : When solving a puzzle, the user can use this command to receive a hint.
                 
                 ITEMS:
-                "pickup" + item name : Users can use this command to read a description of an item located in the room or inventory.
+                "pickup " + item name : Users can use this command to read a description of an item located in the room or inventory.
                 "drop " + item name : Users can use this command to drop an item followed by the name of the item.
                 "equip " + item name : Users can use this command to unequip an item followed by the name of the item. The item returns to the character's inventory.
                 "use " + item name : Users can use this command to use an item followed by the name of the item.
                 "drink " + item name : Users can use this command to drink an item followed by the name of the item.
                 "inspectroom" : When in a room, the user can use this command to see a list of all contents in the current room.
                 "exitroom" : When in a room, the user can use this command to return to the previous room.
-                "turn on lights" : When in a room with lights, the user can use this command to turn on the lights.
+                "lights" : When in a room with lights, the user can use this command to turn on the lights.
                 
                 NPCS:
                 "talk" : User can use this command to interact with the NPCs and receive riddles.
                 "shop" : User can use this command for the purchase of consumables.
                 "riddle" : User can use this command to initiate a riddle.
                 "leave" : User can use this command to leave the shop and end the conversion with the NPC.
-                "buy" + item name + quantity: The user can use this commands in the shop to but consumables.
+                "buy " + item name + quantity: The user can use this commands in the shop to but consumables.
                 =======================================================================================================================================================================================                                                                          
                 """;
     }
