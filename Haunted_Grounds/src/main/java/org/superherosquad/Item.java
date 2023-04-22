019538163e3c1343a17a902cc1bae57dfdbcdd4a
ReAnn <rhollins8199@yahoo.com>
2023-04-22 16:21:37 -0400

    Got newItem method working in Reader.java. Plus had to update the Item, Consumable, and Equipment java classes to appropriately match with the items.txt file tokens.

--- Haunted_Grounds/src/main/java/org/superherosquad/Item.java
+++ Haunted_Grounds/src/main/java/org/superherosquad/Item.java
@@ -3,18 +3,6 @@ package org.superherosquad;
 public class Item {
 
     protected int Id;
-    protected String name;
-    protected String description;
-
-    protected int price;
-
-    public Item(int Id, String name, String description, int price)
-    {
-        this.Id = Id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
 
     public int getId() {
         return Id;
@@ -40,6 +28,30 @@ public class Item {
         this.description = description;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public int getEffect() {
+        return effect;
+    }
+
+    public void setEffect(int effect) {
+        this.effect = effect;
+    }
+
     public int getPrice() {
         return price;
     }
@@ -48,11 +60,42 @@ public class Item {
         this.price = price;
     }
 
+    public int[] getRoomAssociations() {
+        return roomAssociations;
+    }
+
+    public void setRoomAssociations(int[] roomAssociations) {
+        this.roomAssociations = roomAssociations;
+    }
+
+    protected String name;
+    protected String description;
+    protected String type;
+    protected String command;
+    protected int effect;
+    protected int price;
+    protected int[] roomAssociations;
+
+    public Item(int Id, String name, String description, String type, String command, int effect, int price, int[] roomAssociations)
+    {
+        this.Id = Id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.command = command;
+        this.effect = effect;
+        this.price = price;
+        this.roomAssociations = roomAssociations;
+    }
+
+
+
     /*/
     /todo: complete the method.
     public void use()
     {
     }*/
+
 
     @Override
     public String toString() {
